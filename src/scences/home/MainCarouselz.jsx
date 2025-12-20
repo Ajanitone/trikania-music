0034f83e1daf262b2d3e2f4599da6ab3109aba03
kirk mcdowell <ajanitone@freenet.de>
2025-12-20 20:19:30 +0100

    Serve references carousel images from public/references

--- src/scences/home/MainCarouselz.jsx
+++ src/scences/home/MainCarouselz.jsx
@@ -1,107 +1,121 @@
 import React from "react";
-import {
-  Box,
-  Typography,
-  IconButton,
-  useMediaQuery,
-  Button,
-} from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import { Carousel } from "react-responsive-carousel";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the dark mode CSS file
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { shades } from "../../theme";
-import { useNavigate } from "react-router-dom";
 
-// import all image from assets folder
+// Serve from public/references to avoid hashed bundle caching issues.
+const refImages = [
+  "/references/alaine.jpeg",
+  "/references/aldubb.jpg",
+  "/references/andrew-donalds.jpeg",
+  "/references/anthony-locks.jpeg",
+  "/references/asassin.jpeg",
+  "/references/ben.jpeg",
+  "/references/brothers-keepers.jpeg",
+  "/references/bushido.jpeg",
+  "/references/caramela criminal.jpeg",
+  "/references/chris cosmo.jpeg",
+  "/references/congo-ashantiroy.jpeg",
+  "/references/cornel-campbell.jpeg",
+  "/references/d-flame.jpeg",
+  "/references/doniki.jpeg",
+  "/references/dutchie-gold.jpeg",
+  "/references/emersound.jpeg",
+  "/references/fanta-vier.jpeg",
+  "/references/far-east-band.jpeg",
+  "/references/frankie-paul.jpeg",
+  "/references/ganjaman.jpeg",
+  "/references/general-degree.jpeg",
+  "/references/glen-washington.jpeg",
+  "/references/half-pint.jpeg",
+  "/references/hortense-elllis.jpeg",
+  "/references/ijahman.jpeg",
+  "/references/iqullah.jpeg",
+  "/references/jah-mason.jpeg",
+  "/references/jan-delay.jpeg",
+  "/references/jaqee.jpeg",
+  "/references/joseph-grant.jpeg",
+  "/references/junior-kellly.jpeg",
+  "/references/kaya-t.jpeg",
+  "/references/kesheira.jpeg",
+  "/references/queen-omega.jpeg",
+  "/references/u-brown.jpeg",
+  "/references/yah-meek.jpeg",
+];
 
-const importAll = (r) =>
-  r.keys().reduce((acc, item) => {
-    acc[item.replace("./", "")] = r(item);
-    return acc;
-  }, {});
-
-const heroTextureImports = importAll(
-  require.context("../../assets-3", false, /\.(png|jpe?g|svg)$/)
-);
-const MainCarouselz = ({ isDarkMode, toggleTheme }) => {
-  const isNonMobile = useMediaQuery("(min-width:600px)");
-
-  const navigate = useNavigate();
+const MainCarouselz = ({ isDarkMode }) => {
   return (
     <Box
-    sx={{
-      width: "300px", // Adjust the size of the circle
-      height: "400px",
-      borderRadius: "20px",
-      overflow: "hidden", // Ensures the content stays inside the circle
-      margin: "auto", // Centers the carousel (optional)
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    }}
-  >
-    <Carousel
-      className={`home ${isDarkMode ? "dark-mode" : ""}`}
-      infiniteLoop={true}
-      showThumbs={false}
-      showIndicators={false}
-      showStatus={false}
-      autoPlay={true}
-      interval={9000}
-      stopOnHover={true}
-      renderArrowPrev={(onClickHandler, hasPrev, label) => (
-        <IconButton
-          onClick={() => {
-            onClickHandler();
-          }}
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "0",
-            padding: "5px",
-            zIndex: "10",
-            color: isDarkMode ? "red" : "white",
-          }}
-        >
-          <NavigateBeforeIcon sx={{ fontSize: 40 }} />
-        </IconButton>
-      )}
-      renderArrowNext={(onClickHandler, hasNext, label) => (
-        <IconButton
-          onClick={() => {
-            onClickHandler();
-          }}
-          sx={{
-            position: "absolute",
-            top: "50%",
-            right: "0",
-            color: isDarkMode ? "red" : "white",
-            padding: "5px",
-            zIndex: "10",
-          }}
-        >
-          <NavigateNextIcon sx={{ fontSize: 40 }} />
-        </IconButton>
-      )}
+      sx={{
+        width: "300px",
+        height: "400px",
+        borderRadius: "20px",
+        overflow: "hidden",
+        margin: "auto",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
     >
-      {Object.values(heroTextureImports).map((texture, index) => (
-        <Box key={`carousel-image-${index}`}>
-          <img
-            src={texture}
-            alt={`carousel-${index}`}
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover", // Ensures the image fills the circular area
+      <Carousel
+        className={`home ${isDarkMode ? "dark-mode" : ""}`}
+        infiniteLoop
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        autoPlay
+        interval={9000}
+        stopOnHover
+        renderArrowPrev={(onClickHandler) => (
+          <IconButton
+            onClick={onClickHandler}
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "0",
+              padding: "5px",
+              zIndex: "10",
+              color: isDarkMode ? "red" : "white",
             }}
-          />
-        </Box>
-      ))}
-    </Carousel>
-  </Box>
-  
+          >
+            <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+          </IconButton>
+        )}
+        renderArrowNext={(onClickHandler) => (
+          <IconButton
+            onClick={onClickHandler}
+            sx={{
+              position: "absolute",
+              top: "50%",
+              right: "0",
+              color: isDarkMode ? "red" : "white",
+              padding: "5px",
+              zIndex: "10",
+            }}
+          >
+            <NavigateNextIcon sx={{ fontSize: 40 }} />
+          </IconButton>
+        )}
+      >
+        {refImages.map((src, index) => (
+          <Box key={`carousel-image-${index}`}>
+            <img
+              src={src}
+              alt={`carousel-${index}`}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+              loading="eager"
+            />
+          </Box>
+        ))}
+      </Carousel>
+    </Box>
   );
 };
 
